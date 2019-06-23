21fc412d24db0301ef269f02e92264d63d66acf0
janakhpon <jnovaxer@gmail.com>
2019-06-23 12:35:28 +0630

    GDEV005

--- src/components/Updateprofile.js
+++ src/components/Updateprofile.js
@@ -7,122 +7,100 @@ import "./Updateprofile.css";
 class Updateprofile extends React.Component {
   render() {
     return (
-      <div>
-        <main>
-          <center>
-            <div className="section" />
-            <div className="container container-update-profile">
-              <div className="z-depth-1 grey lighten-4 row card-signup">
-                <form className="col s12" method="post">
-                  <div className="row">
-                    <div className="col s12" />
-                  </div>
-
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input
-                        className="auth-text-color"
-                        type="text"
-                        name="name"
-                        id="name"
-                      />
-                      <label className="auth-text-color" for="name">
-                        Enter your name
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input
-                        className="validate auth-text-color"
-                        type="email"
-                        name="email"
-                        id="email"
-                      />
-                      <label className="auth-text-color" for="email">
-                        Enter your email
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <div className="file-field input-field">
-                        <div className="btn">
-                          <span>Profile Photo</span>
-                          <input type="file" />
-                        </div>
-                        <div className="file-path-wrapper">
-                          <input
-                            className="file-path validate"
-                            type="text"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <div className="file-field input-field">
-                        <div className="btn">
-                          <span>Cover Photo</span>
-                          <input type="file" />
-                        </div>
-                        <div className="file-path-wrapper">
-                          <input
-                            className="file-path validate"
-                            type="text"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input
-                        className="auth-text-color"
-                        type="text"
-                        name="name"
-                        id="name"
-                      />
-                      <label className="auth-text-color" for="name">
-                        Current Address
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input
-                        className="auth-text-color"
-                        type="text"
-                        name="name"
-                        id="name"
-                      />
-                      <label className="auth-text-color" for="name">
-                        Job Position
-                      </label>
-                    </div>
-                  </div>
-                  <br />
-                  <center>
-                    <div className="row">
-                      <button
-                        type="submit"
-                        name="btn_login"
-                        className="col s12 btn btn-large waves-effect indigo"
-                      >
-                        Login
-                      </button>
-                    </div>
-                  </center>
-                </form>
+      <div className="row profile-container">
+        <div className="col m1" />
+        <form className="col s12 col m10 form-container">
+          <div className="row" style={{ paddingTop: "1rem" }}>
+            <div className="input-field col s6">
+              <input placeholder="Mr Janakh" id="name" type="text" />
+              <label for="name">Your name </label>
+            </div>
+            <div className="input-field col s6">
+              <input
+                placeholder="contact@myweb.com"
+                id="email"
+                type="text"
+                className="validate"
+              />
+              <label for="email">Contact email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <div className="file-field input-field">
+                <div className="btn">
+                  <span>Profile Photo</span>
+                  <input type="file" />
+                </div>
+                <div className="file-path-wrapper">
+                  <input className="file-path validate" type="text" />
+                </div>
               </div>
             </div>
-          </center>
-        </main>
+            <div className="input-field col s6">
+              <div className="file-field input-field">
+                <div className="btn">
+                  <span>Profile Photo</span>
+                  <input type="file" />
+                </div>
+                <div className="file-path-wrapper">
+                  <input className="file-path validate" type="text" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                placeholder="NO 452, Myinetharyar,Mawlamyine"
+                id="address"
+                type="text"
+              />
+              <label for="address">Your Address</label>
+            </div>
+            <div className="input-field col s6">
+              <input
+                placeholder="Software Engineer"
+                id="position"
+                type="text"
+              />
+              <label for="position">JOB position</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <textarea
+                id="description"
+                className="materialize-textarea"
+                length="140"
+              />
+              <label for="description">Describe yourself or BIO!</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s6">
+              <button
+                type="submit"
+                name="btn_login"
+                className="col s12 btn btn-large waves-effect indigo"
+              >
+                update
+              </button>
+            </div>
+            <div className="input-field col s6">
+              <button
+                type="reset"
+                name="btn_login"
+                className="col s12 btn btn-large waves-effect indigo"
+              >
+                cancel
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="col m1" />
       </div>
     );
   }
